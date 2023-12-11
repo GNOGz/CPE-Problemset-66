82b5d9aa9385558c37a59bba93235608cd0a5f07
GNOGz <gong6321@gmail.com>
2023-12-11 18:47:27 +0700

    complete_unoptimized

--- excercise 6/ex1.cpp
+++ excercise 6/ex1.cpp
@@ -2,8 +2,33 @@
 #include<string>
 using namespace std;
 
+int function1(string xy){
+    int i = 0, output = 0;
+    while (i < xy.size()){
+        if( xy[i] == 'x'){
+            output++;
+        }
+        else{
+            output--;
+
+        }
+        i++;
+    }
+    if(output == 0){ // มี x = y
+        return 0;
+    }
+    else if(output > 0) // มี x มากกว่า y
+    {
+        return 1;
+    }
+    else{ //มี y มากกว่า x
+        return -1;
+    }
+    return 0;
+}
+
 int main(){
 
-    cout << "Hello world GGEZ";
+    cout << function1("xxyyyyyyyyyxxxxxxxxxxxxx");
     return 0;
 }
