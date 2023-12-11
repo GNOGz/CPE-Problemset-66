a31badd63636716d05c479d16d9be9a6d2a8f1a5
GNOGz <gong6321@gmail.com>
2023-12-11 19:00:20 +0700

    complete

--- excercise 6/ex2.cpp
+++ excercise 6/ex2.cpp
@@ -1,7 +1,18 @@
 #include<iostream>
 using namespace std;
 
-int main(){
+char function1 (char x){
+    
+    if ( x == 'A'||x == 'E' || x == 'I'||x == 'O'||x == 'U'||x == 'a'||x == 'e' || x == 'i'||x == 'o'||x == 'u'){
+        return 'y';
+    }
+    else{
+        return 'n';
+    }
+}
 
+int main(){
+    cout << function1('A')<<endl;
+    cout << function1('b');
     return 0;
 }
