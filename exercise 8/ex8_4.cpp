800a318b2b14acfc8e3a31b9b5f45291d00e3d3e
GNOGz <gong6321@gmail.com>
2023-12-15 23:50:54 +0700

    8_4Complete

--- exercise 8/ex8_4.cpp
+++ exercise 8/ex8_4.cpp
@@ -1,7 +1,32 @@
 #include<iostream>
 using namespace std;
 
+void printPattern2(int row){
+    if(row <= 0) cout <<"Invalid input\n";
+    else {
+        int oCount = 1,xCount = row-1;
+        for(int i = 1;i <= row; i++ ){
+            for(int j = 1 ; j <= oCount ; j++){
+                cout<<"O";
+            }
+            for(int j = 1 ; j <= xCount ; j++){
+                cout<<"x";
+            }
+            oCount++;
+            xCount--;
+            cout << endl;
+        }
+    }
+}
+
 int main(){
-    
+    printPattern2(3);
+    cout << endl;
+    printPattern2(4);
+    cout << endl;
+    printPattern2(5);
+    cout << endl;
+    printPattern2(0);
+    printPattern2(-1);
     return 0;
 }
