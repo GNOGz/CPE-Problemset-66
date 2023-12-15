ff35f0d90cbc3c84917d0500c754d5c5470a1a02
GNOGz <gong6321@gmail.com>
2023-12-15 23:57:56 +0700

    8_5 complete

--- exercise 8/ex8_5.cpp
+++ exercise 8/ex8_5.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 void printPattern2(int row){
-    if(row <= 0) cout <<"Invalid input\n";
+    if(row <= 0) cout <<"Invalid input";
     else {
         int oCount = 1,xCount = row-1;
         for(int i = 1;i <= row; i++ ){
@@ -27,6 +27,7 @@ int main(){
     printPattern2(5);
     cout << endl;
     printPattern2(0);
+    cout << endl;
     printPattern2(-1);
     return 0;
 }
