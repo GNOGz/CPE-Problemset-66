bb33a7c3502c5170d5b3fa9108d7fe53afc9ecd1
GNOGz <gong6321@gmail.com>
2023-12-16 00:08:31 +0700

    8_4 complete

--- exercise 8/ex8_4.cpp
+++ exercise 8/ex8_4.cpp
@@ -1,32 +1,39 @@
 #include<iostream>
 using namespace std;
 
-void printPattern2(int row){
-    if(row <= 0) cout <<"Invalid input\n";
-    else {
-        int oCount = 1,xCount = row-1;
-        for(int i = 1;i <= row; i++ ){
-            for(int j = 1 ; j <= oCount ; j++){
-                cout<<"O";
+void printPattern1(int N,int M){
+    if(N <= 0 || M <= 0){
+       cout <<"Invalid input"; 
+    }
+    else{
+        for(int i = 1;i <= N ; i++){
+            if(!(i%2)){
+                for(int j = 1 ; j<= M; j++){ //บรรทัดที่เป็นเลขคี่จะเริ่ม x
+                    if(!(j%2)) cout << 'O'; 
+                    else cout << 'x';
+                }
+                    }
+            else{
+                for(int j = 1 ; j<= M; j++){ //บรรทัดที่เป็นเลขคู่จะเริ่มด้วย O
+                    if(!(j%2)) cout << 'x';
+                    else cout << 'O';
+                }
             }
-            for(int j = 1 ; j <= xCount ; j++){
-                cout<<"x";
-            }
-            oCount++;
-            xCount--;
             cout << endl;
         }
+
     }
 }
 
 int main(){
-    printPattern2(3);
+    printPattern1(2,2);
     cout << endl;
-    printPattern2(4);
+    printPattern1(3,5);
     cout << endl;
-    printPattern2(5);
+    printPattern1(5,3);
     cout << endl;
-    printPattern2(0);
-    printPattern2(-1);
+    printPattern1(0,3);
+    cout << endl;
+    printPattern1(7,-1);
     return 0;
 }
