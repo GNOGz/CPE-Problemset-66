44657ce5a88229dcdbbc873a16bb304f93259712
GNOGz <gong6321@gmail.com>
2023-12-16 00:14:29 +0700

    8_1 complete

---
+++ exercise 8/ex8_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<iomanip>
+using namespace  std;
+int main(){
+    cout << fixed << setprecision(10) ; // ตั้งให้แสดงทศนิยม10ตำแหน่ง
+    double sum = 0;
+    for(double i = 2;i <= 222 ;i+=2){
+        sum = sum+(1/i);
+    }
+    cout << "sum = " << sum;
+return 0;
+}
