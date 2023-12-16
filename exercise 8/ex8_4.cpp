3568c34d29984996979ccc646d1e364cfca86d8d
archiash <archiash.legacy@gmail.com>
2023-12-16 09:19:39 +0700

    Add Another Approach to Exercise 8_4 and 8_5

--- exercise 8/ex8_4.cpp
+++ exercise 8/ex8_4.cpp
@@ -1,39 +1,87 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void printPattern1(int N,int M){
-    if(N <= 0 || M <= 0){
-       cout <<"Invalid input"; 
+void printPattern1(int N, int M)
+{
+    if (N <= 0 || M <= 0)
+    {
+        cout << "Invalid input";
     }
-    else{
-        for(int i = 1;i <= N ; i++){
-            if(!(i%2)){
-                for(int j = 1 ; j<= M; j++){ //บรรทัดที่เป็นเลขคี่จะเริ่ม x
-                    if(!(j%2)) cout << 'O'; 
-                    else cout << 'x';
+    else
+    {
+        for (int i = 1; i <= N; i++)
+        {
+            if (!(i % 2))
+            {
+                for (int j = 1; j <= M; j++)
+                { // บรรทัดที่เป็นเลขคี่จะเริ่ม x
+                    if (!(j % 2))
+                        cout << 'O';
+                    else
+                        cout << 'x';
                 }
-                    }
-            else{
-                for(int j = 1 ; j<= M; j++){ //บรรทัดที่เป็นเลขคู่จะเริ่มด้วย O
-                    if(!(j%2)) cout << 'x';
-                    else cout << 'O';
+            }
+            else
+            {
+                for (int j = 1; j <= M; j++)
+                { // บรรทัดที่เป็นเลขคู่จะเริ่มด้วย O
+                    if (!(j % 2))
+                        cout << 'x';
+                    else
+                        cout << 'O';
                 }
             }
             cout << endl;
         }
-
     }
 }
 
-int main(){
-    printPattern1(2,2);
+void printPattern1ButOtherApproach(int N, int M)
+{
+    if (N <= 0 || M <= 0)
+    {
+        cout << "Invalid input" << endl;
+        return;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            if ((i + j) % 2)
+                cout << "X";
+            else
+                cout << "O";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
+    printPattern1(2, 2);
     cout << endl;
-    printPattern1(3,5);
+    printPattern1ButOtherApproach(2, 2);
+    cout << "________________" << endl;
+
+    printPattern1(3, 5);
     cout << endl;
-    printPattern1(5,3);
+    printPattern1ButOtherApproach(3, 5);
+    cout << "________________" << endl;
+
+    printPattern1(5, 3);
     cout << endl;
-    printPattern1(0,3);
+    printPattern1ButOtherApproach(5, 3);
+    cout << "________________" << endl;
+
+    printPattern1(0, 3);
     cout << endl;
-    printPattern1(7,-1);
+    printPattern1ButOtherApproach(0, 3);
+    cout << "________________" << endl;
+
+    printPattern1(7, -1);
+    cout << endl;
+    printPattern1ButOtherApproach(7, -1);
+    cout << "________________" << endl;
     return 0;
 }
--- exercise 8/ex8_5.cpp
+++ exercise 8/ex8_5.cpp
@@ -1,16 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-void printPattern2(int row){
-    if(row <= 0) cout <<"Invalid input";
-    else {
-        int oCount = 1,xCount = row-1;
-        for(int i = 1;i <= row; i++ ){
-            for(int j = 1 ; j <= oCount ; j++){
-                cout<<"O";
+void printPattern2(int row)
+{
+    if (row <= 0)
+        cout << "Invalid input";
+    else
+    {
+        int oCount = 1, xCount = row - 1;
+        for (int i = 1; i <= row; i++)
+        {
+            for (int j = 1; j <= oCount; j++)
+            {
+                cout << "O";
             }
-            for(int j = 1 ; j <= xCount ; j++){
-                cout<<"x";
+            for (int j = 1; j <= xCount; j++)
+            {
+                cout << "x";
             }
             oCount++;
             xCount--;
@@ -19,15 +25,49 @@ void printPattern2(int row){
     }
 }
 
-int main(){
+void printPattern2ButOtherApproach(int N)
+{
+    if (N <= 0)
+    {
+        cout << "Invalid input" << endl;
+        return;
+    }
+
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            if (j <= i)
+                cout << "O";
+            else
+                cout << "X";
+        }
+        cout << endl;
+    }
+}
+
+int main()
+{
     printPattern2(3);
     cout << endl;
+    printPattern2ButOtherApproach(3);
+    cout << "______________" << endl;
+
     printPattern2(4);
     cout << endl;
+    printPattern2ButOtherApproach(4);
+    cout << "______________" << endl;
     printPattern2(5);
     cout << endl;
+    printPattern2ButOtherApproach(5);
+    cout << "______________" << endl;
     printPattern2(0);
     cout << endl;
+    printPattern2ButOtherApproach(0);
+    cout << "______________" << endl;
     printPattern2(-1);
+    cout << endl;
+    printPattern2ButOtherApproach(-1);
+    cout << "______________" << endl;
     return 0;
 }
