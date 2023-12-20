1be4603c84005db1738c078184c3e3c415040a9c
GNOGz <gong6321@gmail.com>
2023-12-20 20:54:17 +0700

    AddFile

---
+++ excercise 9/ex9_1.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int N;
+    cout << "please input board size: " ;
+    cin >> N;
+    
+    for (int i = 1;____;i++){
+        for(int j = 1;____;j++){
+            cout <<" ";
+            if(j < ___ ) cout <<"|";
+        }
+    }
+    cout << "\n";
+    ____ (i < N){
+        for(int j = 1;j<=N;j++){
+            cout << ____ ;
+            if(____ < N) cout << "+";
+        }
+        cout __;
+    }
+
+    return 0;
+}
---
+++ excercise 9/ex9_1_template.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // อันนี้โค้ดเปล่าที่จารย์ให้ไว้ เผื่อใครขก.พิมพ์ตาม 
+    int N;
+    cout << "please input board size: " ;
+    __________
+    
+    for (int i = 1;____;i++){
+        for(int j = 1;____;j++){
+            cout <<" ";
+            if(j < ___ ) cout <<"|";
+        }
+    }
+    cout << "\n";
+    ____ (i < N){
+        for(int j = 1;j<=N;j++){
+            cout << ____ ;
+            if(____ < N) cout << "+";
+        }
+        cout __;
+    }
+
+    return 0;
+}
