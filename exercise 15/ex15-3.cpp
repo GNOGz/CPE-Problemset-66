250c328fe7b6f68eaaf0a7f867dceb92372cf0ff
archiash <46347714+archiash@users.noreply.github.com>
2024-02-25 09:11:37 +0700

    EMax MIn MAxcdj

---
+++ exercise 15/ex15-3.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    int N, M;
+    cin >> N >> M;
+    int* V = new int[N + M -1];
+    V[0] = 1;
+    for(int i = 1; i < M + N - 1; i++){
+        if(i < max(N, M)) V[i] = V[i - 1] + min(i + 1, min(N, M));
+        else V[i] = V[i - 1] + min(N, M) - (i - max(N, M) + 1);
+        if(i % 2 == 0) V[i-1] = V[i - 2] + 1;
+    }
+    for(int i = 0; i < M; i++){
+        for(int j = 0; j < N; j ++){
+            if((i+j) % 2) cout<< setw(8) << V[i + j]++;
+            else cout<< setw(8) << V[i + j]-- ;
+        }
+        cout << "\n";
+    }
+}
